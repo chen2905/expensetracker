acd89e4879709258313cebf8fb4a3e8ba0292956
CG <maxlin2905@gmail.com>
2020-07-24 22:30:20 +1000

    1030

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 export const AddTransaction = () => {
+    const [text,setText]= useState('')
+    const [amount,setAmount] =useState(0)
     return (
         <>
              <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
-          <label HtmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <label htmlFor="text">Text</label>
+          <input type="text" id="text" value={text} onChange={(e)=> setText(e.target.value)} 
+          placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label HtmlFor="amount"
+          <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number"  placeholder="Enter amount..." />
+          <input type="number"  placeholder="Enter amount..." value={amount} 
+          onChange={(e)=> setAmount(e.target.value)}/>
         </div>
         <button className="btn">Add transaction</button>
       </form>
         </>
     )
 }
+  
--- expense-tracker-react/src/components/Balance.js
+++ expense-tracker-react/src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
 export const Balance = () => {
+    const {transactions} =useContext(GlobalContext)
+
+
+    const amounts =transactions.map(transaction=>transaction.amount)
+    const balance =amounts.reduce((total,amount)=>{
+        return total+=amount
+    },0).toFixed(2)
     return (
         <>
             <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+    <h1 id="balance">${balance}</h1>
         </>
     )
 }
